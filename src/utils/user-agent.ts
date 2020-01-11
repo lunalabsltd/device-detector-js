2dd6a99c88d9e9910fd559181a68740d195111e3
Anton Zhuravsky <anton@lunalabs.uk>
2020-01-11 17:15:32 +0300

    Adding cache to reuse regular expressions instead of constantly recompiling them

--- src/utils/user-agent.ts
+++ src/utils/user-agent.ts
@@ -1,9 +1,20 @@
-export const userAgentParser = (rawRegex: string, userAgent: string): string[] | null => {
-  const regex = `(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(?:${rawRegex})`;
+const regexCache: Record<string, RegExp> = {};
 
+export const userAgentParser = (rawRegex: string, userAgent: string): string[] | null => {
   // TODO: find out why it fails in some browsers
   try {
-    const match = RegExp(regex, "i").exec(userAgent);
+    // try fetching the compiled regex from cache
+    let regex = regexCache[ rawRegex ];
+
+    // check if we got anything out
+    if (!regex) {
+      // we didn't - let's create a new regex instance
+      regexCache[ rawRegex ] = RegExp(`(?:^|[^A-Z0-9-_]|[^A-Z0-9-]_|sprd-)(?:${rawRegex})`, "i");
+      regex = regexCache[ rawRegex ];
+    }
+
+    // match the string against the regex
+    const match = regex.exec(userAgent);
 
     return match ? match.slice(1) : null;
   } catch {
